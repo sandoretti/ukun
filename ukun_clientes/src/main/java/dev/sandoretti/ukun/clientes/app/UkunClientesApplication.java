b628cf4cada10cff6f049341ca511cb326639922
Italo Joel Sandoval Amoretti <italo.sandoval@edu.uah.es>
2024-03-06 15:20:31 +0100

    Init aplicacion de clientes

---
+++ ukun_clientes/src/main/java/dev/sandoretti/ukun/clientes/app/UkunClientesApplication.java
@@ -0,0 +1,13 @@
+package dev.sandoretti.ukun.clientes.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkunClientesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkunClientesApplication.class, args);
+    }
+
+}
---
+++ ukun_clientes/src/test/java/dev/sandoretti/ukun/clientes/app/UkunClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sandoretti.ukun.clientes.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkunClientesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
