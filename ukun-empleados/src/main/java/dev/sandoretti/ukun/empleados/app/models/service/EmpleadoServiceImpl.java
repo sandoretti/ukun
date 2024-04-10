023d35a73c59acdde485859a9b3a2607ffd64e39
Italo Joel Sandoval Amoretti <italo.sandoval@edu.uah.es>
2024-04-10 23:26:26 +0200

    Aplicando las notaciones Transactional
    Se ha aplicado transactional a todas las funciones de los servicios que interactuen con la base de datos mediante los CRUD

--- ukun-empleados/src/main/java/dev/sandoretti/ukun/empleados/app/models/service/EmpleadoServiceImpl.java
+++ ukun-empleados/src/main/java/dev/sandoretti/ukun/empleados/app/models/service/EmpleadoServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,26 +17,31 @@ public class EmpleadoServiceImpl implements IEmpleadoService
     private IEmpleadoCrud empleadoCrud;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Empleado> findAll() {
         return (List<Empleado>) empleadoCrud.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Page<Empleado> findAll(Pageable pageable) {
         return empleadoCrud.findAll(pageable);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Empleado findById(Long id) {
         return empleadoCrud.findById(id).orElse(null);
     }
 
     @Override
+    @Transactional
     public void save(Empleado empleado) {
         empleadoCrud.save(empleado);
     }
 
     @Override
+    @Transactional
     public void delete(Long id) {
         empleadoCrud.deleteById(id);
     }
--- ukun-empleados/src/main/java/dev/sandoretti/ukun/empleados/app/models/service/LoginService.java
+++ ukun-empleados/src/main/java/dev/sandoretti/ukun/empleados/app/models/service/LoginService.java
@@ -4,6 +4,7 @@ import dev.sandoretti.ukun.empleados.app.models.dao.IEmpleadoCrud;
 import dev.sandoretti.ukun.empleados.app.models.entity.Empleado;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class LoginService implements ILoginService
@@ -12,6 +13,7 @@ public class LoginService implements ILoginService
     private IEmpleadoCrud empleadoCrud;
 
     @Override
+    @Transactional(readOnly = true)
     public Empleado loginEmpleado(String correo, String contrasenna) {
         return empleadoCrud.findByCorreoAndContrasenna(correo, contrasenna);
     }
