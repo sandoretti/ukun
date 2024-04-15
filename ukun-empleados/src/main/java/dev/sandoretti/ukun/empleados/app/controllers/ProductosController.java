83ebf79dfff4ec11895cda0c1045471d31a9b7a8
Italo Joel Sandoval Amoretti <italo.sandoval@edu.uah.es>
2024-04-15 12:52:53 +0200

    Iniciado Guardado de nuevos productos

--- ukun-empleados/src/main/java/dev/sandoretti/ukun/empleados/app/controllers/ProductosController.java
+++ ukun-empleados/src/main/java/dev/sandoretti/ukun/empleados/app/controllers/ProductosController.java
@@ -2,19 +2,20 @@ package dev.sandoretti.ukun.empleados.app.controllers;
 
 import dev.sandoretti.ukun.empleados.app.models.entity.Producto;
 import dev.sandoretti.ukun.empleados.app.models.service.IProductoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/admin/productos")
-@SessionAttributes("empleado")
+@SessionAttributes({"empleado", "producto"})
 public class ProductosController
 {
     @Autowired
@@ -30,5 +31,30 @@ public class ProductosController
     public String index()
     {
         return "productos";
+    }
+
+    @GetMapping("/crear")
+    public String crear(Model model)
+    {
+        model.addAttribute("producto", new Producto());
+        return "guardarProducto";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(@Valid @ModelAttribute("producto") Producto producto,
+                          @RequestParam MultipartFile foto,
+                          BindingResult result,
+                          Model model,
+                          RedirectAttributes flash)
+    {
+        if (result.hasErrors())
+        {
+            flash.addFlashAttribute("error", "Hubo un error al guardar el producto");
+            return "guardarProducto";
+        }
+
+        return "redirect:/admin/productos";
+
+
     }
 }
