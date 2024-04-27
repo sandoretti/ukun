5b186591bcc552740b78c4937d181422f83e4620
Italo Joel Sandoval Amoretti <italo.sandoval@edu.uah.es>
2024-04-27 15:11:35 +0200

    Implementado cliente en controlador Home

--- ukun-clientes/src/main/java/dev/sandoretti/ukun/clientes/app/controllers/HomeController.java
+++ ukun-clientes/src/main/java/dev/sandoretti/ukun/clientes/app/controllers/HomeController.java
@@ -1,13 +1,27 @@
 package dev.sandoretti.ukun.clientes.app.controllers;
 
+import dev.sandoretti.ukun.clientes.app.models.entity.Cliente;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
-public class HomeController {
+@SessionAttributes("cliente")
+@RequestMapping("/")
+public class HomeController
+{
+    @ModelAttribute
+    private Cliente cliente(Model model)
+    {
+        return (Cliente) model.getAttribute("cliente");
+    }
+
     @GetMapping(value = {"/", ""})
-    public String home(Model model){
+    public String home(Model model)
+    {
         return "index";
     }
 }
