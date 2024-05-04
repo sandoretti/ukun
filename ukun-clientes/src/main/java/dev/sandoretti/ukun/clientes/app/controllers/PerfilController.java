f00f098124c06bf84ad10c4ef3c1e2ff09123a31
Italo Joel Sandoval Amoretti <italo.sandoval@edu.uah.es>
2024-05-04 20:02:17 +0200

    Implementado Controlador del perfil
    Se pueden visualizar los campos del perfil del cliente y editarlos, a excepcion de la  contraseña

--- ukun-clientes/src/main/java/dev/sandoretti/ukun/clientes/app/controllers/PerfilController.java
+++ ukun-clientes/src/main/java/dev/sandoretti/ukun/clientes/app/controllers/PerfilController.java
@@ -1,13 +1,61 @@
 package dev.sandoretti.ukun.clientes.app.controllers;
 
+import dev.sandoretti.ukun.clientes.app.models.entity.Cliente;
+import dev.sandoretti.ukun.clientes.app.services.IClienteService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@SessionAttributes("cliente")
 @RequestMapping("/perfil")
-public class PerfilController
+public class PerfilController extends AbsController
 {
+    @Autowired
+    private IClienteService clienteService;
 
+    @GetMapping(value = {"", "/"})
+    public String perfil()
+    {
+        return "perfil";
+    }
+
+    @PostMapping("/editar")
+    public String editar(@Valid @ModelAttribute("cliente") Cliente clienteModificado,
+                             BindingResult result,
+                             Model model,
+                             RedirectAttributes flash)
+    {
+        // Obtenenos el cliente correspondiente a la base de datos
+        Cliente clienteActual = clienteService.findById(clienteModificado.getId());
+
+        // Si el cliente es el mismo, volvemos a la pagina de perfil
+        if (clienteActual.equals(clienteModificado))
+        {
+            return "redirect:/perfil";
+        }
+
+        // Comprobamos si no tiene errores de validacion, si lo tiene, redirigimos con mensaje de error
+        if (result.hasErrors())
+        {
+            flash.addFlashAttribute("org.springframework.validation.BindingResult.cliente", result);
+            flash.addFlashAttribute("error", "Error al editar la cuenta");
+
+            model.addAttribute("cliente", clienteActual);
+
+            return "redirect:/perfil";
+        }
+
+        // Guardamos el cliente modificado en la base de datos
+        clienteService.save(clienteModificado);
+
+        // Mandamos un mensaje de exito
+        flash.addFlashAttribute("success", "Campos modificados con exito");
+
+        return "redirect:/perfil";
+
+    }
 }
---
+++ ukun-clientes/src/main/java/dev/sandoretti/ukun/clientes/app/services/ClienteServiceImpl.java
@@ -0,0 +1,27 @@
+package dev.sandoretti.ukun.clientes.app.services;
+
+import dev.sandoretti.ukun.clientes.app.models.dao.IClienteDAO;
+import dev.sandoretti.ukun.clientes.app.models.entity.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClienteServiceImpl implements IClienteService
+{
+    @Autowired
+    private IClienteDAO clienteDAO;
+
+    @Override
+    @Transactional(readOnly = true)
+    public Cliente findById(Long id)
+    {
+        return clienteDAO.findById(id).orElse(null);
+    }
+
+    @Override
+    public void save(Cliente cliente)
+    {
+        clienteDAO.save(cliente);
+    }
+}
