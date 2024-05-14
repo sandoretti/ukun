5c124d0d5f7f5ebec010d8f6ac323b518a0b7315
Italo Joel Sandoval Amoretti <italo.sandoval@edu.uah.es>
2024-05-14 03:51:12 +0200

    Añadir stock de productos que no se encuentren en la tienda

--- ukun-empleados/src/main/java/dev/sandoretti/ukun/empleados/app/controllers/StockController.java
+++ ukun-empleados/src/main/java/dev/sandoretti/ukun/empleados/app/controllers/StockController.java
@@ -2,6 +2,7 @@ package dev.sandoretti.ukun.empleados.app.controllers;
 
 import dev.sandoretti.ukun.empleados.app.models.embeddable.StockProductoId;
 import dev.sandoretti.ukun.empleados.app.models.entity.Empleado;
+import dev.sandoretti.ukun.empleados.app.models.entity.Producto;
 import dev.sandoretti.ukun.empleados.app.models.entity.StockProducto;
 import dev.sandoretti.ukun.empleados.app.models.entity.Tienda;
 import dev.sandoretti.ukun.empleados.app.models.service.IStockService;
@@ -24,32 +25,22 @@ public class StockController
 
     /**
      * Establece la tienda correspondiente al usuario
-     * @param model Modelo
+     * @param empleado Empleado
      * @return Tienda del empleado
      */
     @ModelAttribute("tienda")
-    public Tienda tienda(Model model)
+    public Tienda tienda(@ModelAttribute("empleado") Empleado empleado)
     {
-        Empleado empleado = (Empleado) model.getAttribute("empleado");
         return empleado.getTienda();
     }
 
-    /**
-     * Establece la lista de los productos con stock correspondientes a la tienda
-     * @param model Model
-     * @return Lista de productos con stock
-     */
-    @ModelAttribute("stockProductosList")
-    public List<StockProducto> stockProductos(Model model)
-    {
-        Tienda tienda = (Tienda) model.getAttribute("tienda");
-
-        return stockService.findStockProductoByTienda(tienda);
-    }
-
     @GetMapping(value = {"", "/"})
-    public String menu()
+    public String menu(@ModelAttribute("tienda") Tienda tienda,
+                       Model model)
     {
+        model.addAttribute("stockProductosList", stockService.findStockProductoByTienda(tienda));
+        model.addAttribute("productoNoTienda", stockService.findProductoNotInTienda(tienda));
+
         return "stock";
     }
 
@@ -57,14 +48,14 @@ public class StockController
      * Eliminar el stockProducto de la base de datos
      * @param tiendaId Identificador de la tienda
      * @param productoId Identificador del producto
-     * @param model Modelo
+     * @param tienda Tienda del empleado
      * @param flash Atributos de redireccion
      * @return Redireccion
      */
     @GetMapping("/eliminar")
     public String eliminar(@RequestParam Long tiendaId,
                            @RequestParam Long productoId,
-                           Model model,
+                           @ModelAttribute("tienda") Tienda tienda,
                            RedirectAttributes flash)
     {
         // Comprobamos si los parametros no son nulos, si no lo son mandamos a stock con mensaje de error
@@ -75,7 +66,7 @@ public class StockController
         }
 
         // Comprobamos que el id de la tienda es igual que el id pasado, si no lo son mandamos a stock con mensaje de error
-        if (!Objects.equals(tienda(model).getId(), tiendaId))
+        if (!Objects.equals(tienda.getId(), tiendaId))
         {
             flash.addFlashAttribute("error", "No pertenece a la tienda correspondiente");
             return "redirect:/stock";
@@ -95,13 +86,12 @@ public class StockController
         return "redirect:/stock";
     }
 
-
     /**
      * Permite editar el stock del StockProducto
      * @param tiendaId Identificador de la tienda
      * @param productoId Identificador del producto
      * @param stock Stock a cambiar
-     * @param model Modelo
+     * @param tienda Tienda del cliente
      * @param flash Atributos de redireccion
      * @return Redireccion
      */
@@ -109,7 +99,7 @@ public class StockController
     public String editar(@RequestParam Long tiendaId,
                          @RequestParam Long productoId,
                          @RequestParam Long stock,
-                         Model model,
+                         @ModelAttribute("tienda") Tienda tienda,
                          RedirectAttributes flash)
     {
         // Comprobamos si los parametros no son nulos, si no lo son mandamos a stock con mensaje de error
@@ -120,28 +110,23 @@ public class StockController
         }
 
         // Comprobamos que el id de la tienda es igual que el id pasado, si no lo son mandamos a stock con mensaje de error
-        if (!Objects.equals(tienda(model).getId(), tiendaId))
+        if (!Objects.equals(tienda.getId(), tiendaId))
         {
             flash.addFlashAttribute("error", "No pertenece a la tienda correspondiente");
             return "redirect:/stock";
         }
 
-        // Creamos el id de stock producto con los campos requeridos
-        StockProductoId stockProductoId = new StockProductoId();
+        if (stock < 0)
+        {
+            flash.addFlashAttribute("error", "El stock es negativo");
+            return "redirect:/stock";
+        }
 
-        stockProductoId.setProductoId(productoId);
-        stockProductoId.setTiendaId(tiendaId);
+        if (!stockService.guardar(tienda, productoId, stock))
+            flash.addFlashAttribute("error", "Error al guardar el stock");
+        else
+            flash.addFlashAttribute("success", "Stock guardado exitosamente");
 
-        // Creamos el stockProducto a insertar en la base de datos
-        StockProducto stockProducto = new StockProducto();
-
-        stockProducto.setId(stockProductoId);
-        stockProducto.setStock(stock);
-
-        stockService.save(stockProducto);
-
-        // Mandamos mensaje de exito, junto a redireccion a stock
-        flash.addFlashAttribute("success", "Stock editado exitosamente");
         return "redirect:/stock";
     }
 }
