b5458ba28b7f98a4d9fc073ef986c6bfa479d1c5
Italo Joel Sandoval Amoretti <italo.sandoval@edu.uah.es>
2024-05-18 01:21:21 +0200

    Incluido margen debajo de las tablas y tiempo para las alertas

--- ukun-empleados/src/main/resources/static/js/scripts.js
+++ ukun-empleados/src/main/resources/static/js/scripts.js
@@ -7,3 +7,18 @@ function formatearPrecios() {
         price.textContent = value;
     });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        const successMessages = document.querySelectorAll('.alert-success');
+        const errorMessages = document.querySelectorAll('.alert-danger');
+
+        successMessages.forEach(function(message) {
+            message.style.display = 'none';
+        });
+
+        errorMessages.forEach(function(message) {
+            message.style.display = 'none';
+        });
+    }, 4000); // 5000 ms = 5 segundos
+});
