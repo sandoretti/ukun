f64167b56976a38f677fb8fca1c17f65f45100e2
Italo Joel Sandoval Amoretti <italo.sandoval@edu.uah.es>
2024-05-18 17:32:51 +0200

    Arreglado error de JSESSIONID

--- ukun-clientes/src/main/java/dev/sandoretti/ukun/clientes/app/controllers/HomeController.java
+++ ukun-clientes/src/main/java/dev/sandoretti/ukun/clientes/app/controllers/HomeController.java
@@ -5,15 +5,13 @@ import dev.sandoretti.ukun.clientes.app.services.ILoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@SessionAttributes("cliente")
 @RequestMapping("/")
-public class HomeController
+public class HomeController extends AbsController
 {
     @Autowired
     private ILoginService loginService;
